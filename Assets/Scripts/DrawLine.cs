ca4f8b970b2abb66c3c161f4f6aa9b4ee4f37b87
ErenUtku <61837087+ErenUtku@users.noreply.github.com>
2021-12-14 20:44:22 +0300

    Init

---
+++ Assets/Scripts/DrawLine.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{
+    public GameObject linePrefab;
+    public GameObject parachute;
+    public GameObject InstantiatedCubes;
+    public EdgeCollider2D edgeCollider;
+
+    private GameObject currentLine;
+
+    public LineRenderer lineRenderer;
+
+    public List<Vector2> drawPositions;
+
+    public bool creatingObject;
+
+    public GameObject drawing;
+
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (mousePos.x < 5 && mousePos.x > -5 && mousePos.y > -3.5f && mousePos.y < -0.5f)
+        {           
+            if (Input.GetMouseButtonDown(0))
+            {
+                creatingObject = true;
+                CreateLineFunction();
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                creatingObject = false;
+                if (Vector2.Distance(mousePos, drawPositions[drawPositions.Count - 1]) > .1f)
+                {
+                    UpdateLineFunction(mousePos);                 
+                }
+                
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                creatingObject = true;
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                StartCoroutine(cubesCoroutine());
+            }
+        }
+
+        else
+        {
+            drawing = GameObject.Find("Line(Clone)");
+            if (drawing && !creatingObject)
+            {
+                Debug.Log("YOU ARE OUT OF THE PALETTE, DRAW AGAIN!");
+                Destroy(drawing);
+                drawPositions.Clear();
+            }
+        }
+    }
+
+    void CreateLineFunction()
+    {
+        currentLine = Instantiate(linePrefab, Vector3.zero , Quaternion.identity);
+        lineRenderer = currentLine.GetComponent<LineRenderer>();
+        edgeCollider = currentLine.GetComponent<EdgeCollider2D>();
+        drawPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        drawPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        lineRenderer.SetPosition(0, drawPositions[0]);
+        lineRenderer.SetPosition(1, drawPositions[1]);
+        edgeCollider.points = drawPositions.ToArray();
+    }
+
+    void UpdateLineFunction(Vector2 newDrawPos)
+    {
+        drawPositions.Add(newDrawPos);
+        lineRenderer.positionCount++;
+        lineRenderer.SetPosition(lineRenderer.positionCount - 1,newDrawPos);
+        edgeCollider.points = drawPositions.ToArray();
+    }
+
+    void CreateCubes()
+    {
+        foreach (Transform child in InstantiatedCubes.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+        
+        for (int i = 0; i < drawPositions.Count; i++)
+        {
+            Vector3 cubesTransform= new Vector3(drawPositions[i].x, drawPositions[i].y + 5, 0);
+            GameObject cubes = Instantiate(parachute, cubesTransform, Quaternion.identity);
+            cubes.transform.parent = GameObject.Find("CubesInstantiate").transform;
+        }
+    }
+
+    IEnumerator cubesCoroutine()
+    {
+        yield return new WaitForSeconds(1f);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        CreateCubes();
+        drawPositions.Clear();
+        Destroy(currentLine);       
+    }
+
+}
